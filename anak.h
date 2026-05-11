31c0029f6a47415ec278c4eecc943a6e72d4accd
nirayndra <faadiajasmine@gmail.com>
2026-05-11 18:02:17 +0700

    membuat classs anak di file header anak.h

--- anak.h
+++ anak.h
@@ -3,14 +3,4 @@
 
 //membuat classs anak di file header anak.h
 class anak {
-public:
-    string nama;
-
-    anak(string pNama) : nama(pNama) {
-        cout << "Anak \""  << nama << "\" ada \n";
-
-
-    }
-
-};
-#endif
+}
