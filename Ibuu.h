236423ae85f08ef9e7bcfaaa16aa1af210199339
nirayndra <faadiajasmine@gmail.com>
2026-05-11 18:27:25 +0700

    deklarfasi member variabel dan membuat constructor dan desructor

--- Ibuu.h
+++ Ibuu.h
@@ -4,4 +4,11 @@
 class anak;
 
 class Ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+    Ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \""  << nama << "\" ada \n";
+    };
+
 };
