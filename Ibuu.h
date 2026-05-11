9482d5cf110e6593fc3e24b461ca5666c8963906
nirayndra <faadiajasmine@gmail.com>
2026-05-11 18:32:22 +0700

    menambah destructor

--- Ibuu.h
+++ Ibuu.h
@@ -7,8 +7,13 @@ class Ibu {
 public:
     string nama;
     vector<anak*> daftar_anak;
+
     Ibu(string pNama) : nama(pNama) {
         cout << "Ibu \""  << nama << "\" ada \n";
+    };
+
+    ~Ibu() {
+        cout << "Ibu \""  << nama << "\" tidak ada \n";
     };
 
 };
