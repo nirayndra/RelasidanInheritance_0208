fe8ca95270adbed5a3d2f12315bc0d569873c651
nirayndra <faadiajasmine@gmail.com>
2026-05-11 19:56:43 +0700

    deklarasi class dan member

--- Asosiasii.cpp
+++ Asosiasii.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//deklarasi class dokter
+class dokter;
+
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    };
+
+    //deklarasi prosedur tambahDokter() dan cetakDokter()
+    void tambahDoker(dokter*);
+    void cetakDokter();
+
+};
