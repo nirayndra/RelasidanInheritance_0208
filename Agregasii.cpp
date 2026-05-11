85c9fd30cbc752cc87d53c5685580ba1dcc6182b
nirayndra <faadiajasmine@gmail.com>
2026-05-11 19:54:09 +0700

    implementasi fungsi utama

--- Agregasii.cpp
+++ Agregasii.cpp
@@ -5,3 +5,21 @@ using namespace std;
 
 #include "anak.h"
 #include "Ibuu.h"
+
+int main()
+{
+   Ibu* varIbu = new Ibu("rani");
+   Ibu* varIbu2 = new Ibu("sari");
+   anak* varAnak1 = new anak("tono");
+   anak* varAnak2 = new anak("tiara");
+   anak* varAnak3 = new anak("dini");
+
+   varIbu->tambahAnak(varAnak1);
+   varIbu->tambahAnak(varAnak2);
+   varIbu2->tambahAnak(varAnak3);
+
+   varIbu->cetakAnak();
+   varIbu2->cetakAnak();
+
+   return 0;
+}
